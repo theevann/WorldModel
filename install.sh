245308f67d9ec783cf67dd29cbc33e27d5247b60
Evann Pierre Guy Courdier <evann.courdier@mines-paristech.fr>
2018-12-18 14:04:24 +0100

    Update install scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 sudo apt-get install -y cmake zlib1g-dev libjpeg-dev libboost-all-dev gcc libsdl2-dev
 conda config --append channels pytorch
-conda create -y -n rl --file requirements.txt
-source activate rl
-pip install -r reqpip.txt
+conda create -y -n rl --file $DIR/requirements.txt
+source activate rl || { echo "ABORTING" && exit 1; }
+pip install -r $DIR/reqpip.txt
